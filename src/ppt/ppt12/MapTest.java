75f567c96f7114781a93601e7b47192fd298b228
kimilguk <boramcom@daum.net>
2022-04-23 16:56:41 +0900

    강의 내용 정리

--- src/ppt/ppt12/MapTest.java
+++ src/ppt/ppt12/MapTest.java
@@ -53,8 +53,6 @@ public class MapTest {
 
              // 하나의 항목을 대치한다.
              st.put("100", new Student(20090003, "최자영"));
-             // 값을 참조한다.
-             System.out.println(st.get("300"));
              // 모든 항목을 방문한다.
              for (Map.Entry<String, Student> s : st.entrySet()) {
                     String key = s.getKey();
@@ -63,5 +61,32 @@ public class MapTest {
                     String name = student.getName();
                     System.out.println("key=" + key + ", name=" + name+ ", number=" + number);
              }
+             // 값을 참조한다.
+             System.out.println(st.get("300"));//맵객체 검색기능: 키 스트링 문자로 값을 가져올 수 있다.
+             // 정렬
+             List<String> keyList = new ArrayList<>(st.keySet());
+             keyList.sort(new Comparator<String>() {
+
+				@Override
+				public int compare(String o1, String o2) {
+	        		return Integer.parseInt(o2) - Integer.parseInt(o1);
+				}
+            	 
+			});
+         // 원본은 그대로 두고 출력순서만 변경한다. 실제 데이터베이스 에서는 내부에서 이렇게 사용된다.
+         for (String key : keyList) {
+        	 	Student student = st.get(key);
+                System.out.println("key=" + key + ", name=" + student.name+ ", number=" + student.number);
+                //원본을 재배치할 수도 있다.
+                //st.put(key, new Student(Integer.parseInt(key), student.name));
+         }
+         
+         for (Map.Entry<String, Student> s : st.entrySet()) {
+             String key = s.getKey();
+             Student student = s.getValue();//객체생성 출력시 toString 메서드 자동실행
+             int number = student.getNumber();
+             String name = student.getName();
+             System.out.println("key=" + key + ", name=" + name+ ", number=" + number);
+         }
        }
 }
