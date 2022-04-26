f83f4021ddd84eb91ef882cba32e798ccb60177f
kimilguk <boramcom@daum.net>
2022-04-26 13:21:03 +0900

    강의 내용 정리

--- src/ppt/ppt01/Hello.java
+++ src/ppt/ppt01/Hello.java
@@ -2,6 +2,7 @@ package ppt.ppt01;
 /**
  * cmd 창에서 컴파일: javac -d . Hello.java -encoding UTF-8
  * cmd 창에서 실행: java ppt.ppt01.Hello
+ * jar파일을 cmd 창에서 실행: javaw -jar -Dfile.encoding=UTF-8 추가
  * @author kimilguk
  *
  */
