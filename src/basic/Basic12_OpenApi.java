f372a8b56958ff2676d5a9bab09a89bb3bc75b0d
kimilguk <boramcom@daum.net>
2022-04-26 12:30:44 +0900

    강의 내용 정리

--- src/basic/Basic12_OpenApi.java
+++ src/basic/Basic12_OpenApi.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -18,9 +20,9 @@ import org.json.JSONObject;
 public class Basic12_OpenApi {
 	//Rest(Representational State Transfer:데이터를 재가공해서 통신)
 	//API(Applicataion Program Interface) 외부연계 역할 메서드(아래)
-	public static void serviceApi() throws IOException {
+	public static JSONArray serviceApi(String urlStr) throws IOException {
 		BufferedReader bufferedReader = null;//전송받은 데이터를 임시 저장하는 공간.자동차범퍼.
-		String urlStr = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20220101";
+		
 		URL url = new URL(urlStr);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");//URL쿼리스트링으로 파라미터를 보낸다.
@@ -37,13 +39,16 @@ public class Basic12_OpenApi {
 		//전체 데이터에서 화면에 출력할 부분 파싱(주의 Json데이터는 Key:Value 로 구성되고, 계층형 구조를 갖는다)
 		JSONObject jsonObject = new JSONObject(sb.toString());
         JSONObject boxOfficeResult = (JSONObject) jsonObject.get("boxOfficeResult");
+        JSONArray dailyBoxOfficeList = (JSONArray) boxOfficeResult.get("dailyBoxOfficeList");
         //Object body = boxOfficeResult.get("dailyBoxOfficeList");
-        System.out.println("파싱 후\n" + boxOfficeResult);//웹 화면에 JSON 객체를 보낸다.
+        //System.out.println("파싱 후\n" + dailyBoxOfficeList);//웹 화면에 JSON 객체를 보낸다.
+        return dailyBoxOfficeList;
 	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		serviceApi();
+		String urlStr = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20220101";
+		serviceApi(urlStr);
 	}
 
 }
--- src/ppt/ppt10/TextAreaFrame.java
+++ src/ppt/ppt10/TextAreaFrame.java
@@ -1,7 +1,40 @@
 ﻿package ppt.ppt10;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import basic.Basic12_OpenApi;
+
+class DailyBoxOffice { //일일 영화 박스오피스 데이터 클래스
+	private String rank;//순위
+	private String audiCnt;//일별관객수
+	private String movieNm;//영화제목
+	
+	public DailyBoxOffice(String rank, String audiCnt, String movieNm) {
+		this.rank = rank;
+		this.audiCnt = audiCnt;
+		this.movieNm = movieNm;
+	}
+
+	@Override
+	public String toString() {
+		return "[rank=" + rank + ", audiCnt=" + audiCnt + ", movieNm=" + movieNm + "]";
+	}
+	
+}
 
 public class TextAreaFrame extends JFrame implements ActionListener {
        protected JTextField textField;
@@ -9,11 +42,11 @@ public class TextAreaFrame extends JFrame implements ActionListener {
  
        public TextAreaFrame() {
              setTitle("Text Area Test");
- 
-             textField = new JTextField(30);
+             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+             textField = new JTextField("http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20220101", 100);
              textField.addActionListener(this);
  
-             textArea = new JTextArea(10, 30);
+             textArea = new JTextArea(10, 100);
              textArea.setEditable(false);
              JScrollPane scrollPane = new JScrollPane(textArea);
 
@@ -28,9 +61,22 @@ public class TextAreaFrame extends JFrame implements ActionListener {
 
        public void actionPerformed(ActionEvent evt) {
              String text = textField.getText();
-             textArea.append(text + "\n");
-             textField.selectAll();
-             textArea.setCaretPosition(textArea.getDocument().getLength());
+             Basic12_OpenApi api = new Basic12_OpenApi();
+             try {
+            	 JSONArray jsonArray = api.serviceApi(text);
+            	 //System.out.println(jsonArray);
+            	 for(Object jsonOne : jsonArray) {
+            		 JSONObject jsonObject = (JSONObject) jsonOne;
+            		 System.out.println(jsonObject.get("rank"));
+            		 DailyBoxOffice dBoxOffice = new DailyBoxOffice(jsonObject.get("rank").toString(),jsonObject.get("audiCnt").toString(),jsonObject.get("movieNm").toString());
+            		 textArea.append(dBoxOffice.toString() + "\n");
+            	 }
+            	 textField.selectAll();
+                 textArea.setCaretPosition(textArea.getDocument().getLength());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
        }
 
        public static void main(String[] args) {
