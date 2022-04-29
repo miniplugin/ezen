2e6093bd126bebfcfe148a5111a40fa2bb681871
kimilguk <boramcom@daum.net>
2022-04-29 10:55:27 +0900

    강의 내용 정리

--- src/ppt/ppt14/LambdaTest.java
+++ src/ppt/ppt14/LambdaTest.java
@@ -1,6 +1,6 @@
 ﻿package ppt.ppt14;
 
-public class LambdaTest {
+public class LambdaTest{
 	public static void main(String args[]) {
 //		
 //		Runnable task = () -> {
@@ -12,12 +12,21 @@ public class LambdaTest {
 			
 			@Override
 			public void run() {
-				for (int i = 10; i >= 0; i--)
-					System.out.print(i + " ");
-				
+				for (int i = 10; i >= 0; i--) {
+					try {
+						Thread.sleep(1000);
+						System.out.print(i + " ");
+						if(i==5) {
+							System.out.println("여기까지");
+							//Thread.currentThread().stop();
+							Thread.currentThread().interrupt();
+						}
+					} catch (InterruptedException e) {
+						break;
+					}
+				}
 			}
 		};
-
 		new Thread(task).start();
 	}
 }
