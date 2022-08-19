bdadfc62facd10cda70e2258da6f90194ef01f9d
miniplugin <52336625+miniplugin@users.noreply.github.com>
2022-08-19 16:06:03 +0900

    Update Dice.java

--- src/miniproject/p04/Dice.java
+++ src/miniproject/p04/Dice.java
@@ -3,13 +3,13 @@ package miniproject.p04;
 public class Dice {
 
 	private int value;
-
+	/* 생성자 메서드가 없어도 컴파일 시 자동 생성 되고, new 키워드로 객체 생성 시 value 값이 0으로 초기화 된다.
 	public Dice() {
 		value = 0;
 	}
-
-	public void roll() {
-		value = (int) (Math.random() * 6) + 1;
+	*/
+	public void roll() { //주사위 굴리기 메서드 구현코드
+		value = (int) (Math.random() * 6) + 1;//랜덤 함수가 0부터 시작하기 때문에 +1 처리
 	}
 
 	public int getValue() {
